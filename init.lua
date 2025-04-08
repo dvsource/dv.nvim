d2dc2fcdbf4367ab0c69c83b4a059b06b1250efb
Viraj Kaushalye <virajkaushalye1@gmail.com>
2025-04-08 10:22:44 +0530

    Initial neovim config with lazy.nvim

---
+++ init.lua
@@ -0,0 +1,36 @@
+require('v3j.set')
+
+--Set
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = false -- Use nerd fonts
+
+vim.opt.autoindent = true    -- take indent for new line from previous line
+vim.opt.background = 'dark'  -- highlight color 'light' / 'dark'
+-- vim.opt.belloff
+-- vim.opt.clipboard = true
+-- vim.opt.confirm
+-- vim.opt.copyindent
+-- vim.opt.cursorbind
+vim.opt.cursorcolumn = false
+vim.opt.cursorline = true
+-- vim.opt.cursorlineopt
+-- vim.opt.debug
+-- vim.opt.define
+-- vim.opt.delcombine
+-- vim.opt.errorbells
+
+
+
+
+
+
+
+
+
+
+
+
+
+require("config.lazy")
---
+++ lua/v3j/set.lua
@@ -0,0 +1,4 @@
+-- v3j.set
+-- Set
+vim.opt.number = true
+vim.opt.relativenumber = true
